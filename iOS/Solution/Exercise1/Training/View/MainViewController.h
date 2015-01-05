c96960cf2ec28200adfe946c07ad3b7c18953b17
JD Benito <jd@hybridforge.com>
2015-01-05 12:23:48 -0700

    Initial commit of Exercise1 - iOS

---
+++ iOS/Solution/Exercise1/Training/View/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  Training
+//
+//  Created by JD Benito on 2015-01-05.
+//  Copyright (c) 2015 Hybrid Forge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController {
+    __weak IBOutlet UILabel *_myTextField;
+}
+
+
+@end
+
