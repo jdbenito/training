8122a199c4acf495a69f8bc0dd1b33ff053f01d8
JD Benito <jd@hybridforge.com>
2015-01-05 15:12:21 -0700

    Preliminary commit of Exercise2 - iOS

---
+++ iOS/Solution/Exercise2/Training/Categories/UILabel+Debugging.h
@@ -0,0 +1,16 @@
+//
+//  UILabel+Debugging.h
+//  Training
+//
+//  Created by JD Benito on 2015-01-05.
+//  Copyright (c) 2015 Hybrid Forge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Debugging)
+
+- (void)showBorders;
+- (void)showBordersUsingColor:(UIColor *)borderColor_;
+
+@end
